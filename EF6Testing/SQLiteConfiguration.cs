47f9da54aa9b27a177c3d4eb37cb7d2ca74079c0
wbskyboy <wbskyboy@126.com>
2024-10-16 19:35:25 +0800

    添加项目文件。

---
+++ EF6Testing/SQLiteConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Data.Entity.Core.Common;
+using System.Data.Entity.Infrastructure;
+using System.Data.SQLite;
+using System.Data.SQLite.EF6;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF6Testing
+{
+	public class SQLiteConfiguration : DbConfiguration , IDbConnectionFactory
+	{
+		public SQLiteConfiguration()
+		{
+			SetProviderFactory("System.Data.SQLite", SQLiteFactory.Instance);
+			SetProviderFactory("System.Data.SQLite.EF6", SQLiteProviderFactory.Instance);
+			var providerServices = (DbProviderServices)SQLiteProviderFactory.Instance.GetService(typeof(DbProviderServices));
+			SetProviderServices("System.Data.SQLite", providerServices);
+			SetProviderServices("System.Data.SQLite.EF6", providerServices);
+
+			SetDefaultConnectionFactory(this);
+		}
+		public DbConnection CreateConnection(string connectionString) => new SQLiteConnection(connectionString);
+	}
+}
---
+++ EF6Testing/SchoolContext.cs
@@ -0,0 +1,51 @@
+﻿using EF6Testing;
+using SQLite.CodeFirst;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.SQLite;
+
+namespace EF6Testing
+{
+	[DbConfigurationType(typeof(SQLiteConfiguration))]
+	public class SchoolContext : DbContext
+    {
+		public SchoolContext() :
+			base(new SQLiteConnection()
+			{
+				ConnectionString = new SQLiteConnectionStringBuilder() { DataSource = "D:\\Databases\\SQLiteWithEF.db", ForeignKeys = true }.ConnectionString
+			}, true)
+		{
+		}
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+			modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            //初始化Sqlite
+			var sqliteConnectionInitializer = new SqliteCreateDatabaseIfNotExists<SchoolContext>(modelBuilder);
+
+			//SqliteCreateDatabaseIfNotExists
+			//SqliteDropCreateDatabaseAlways
+			//SqliteDropCreateDatabaseWhenModelChanges
+			Database.SetInitializer(sqliteConnectionInitializer);
+
+
+			//初始化DbSet<SchoolContext>
+			Database.SetInitializer<SchoolContext>(new SchoolDBInitializer());
+
+			//Adds configurations for Student from separate class
+			modelBuilder.Configurations.Add(new StudentConfigurations());
+
+			//Adds configurations for Student from separate class
+			modelBuilder.Entity<Teacher>()
+				.ToTable("TeacherInfo");
+
+			modelBuilder.Entity<Teacher>()
+				.MapToStoredProcedures();
+
+			base.OnModelCreating(modelBuilder);
+		}
+		public DbSet<Student> Students { get; set; }
+		public DbSet<Grade> Grades { get; set; }
+		public DbSet<Course> Courses { get; set; }
+		public DbSet<StudentAddress> StudentAddresses { get; set; }
+	}
+}
